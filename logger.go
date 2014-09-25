6a86ec821d8689ce6369445963af229b30934574
Donovan Kolbly <donovan.kolbly@civitaslearning.com>
2014-09-25 16:52:58 -0500

    Use standardlog.Logger for TraceLogger()

--- logger.go
+++ logger.go
@@ -1,16 +1,16 @@
 package restful
 
-import "log"
+import "github.com/joefitzgerald/standardlog"
 
 // Copyright 2014 Ernest Micklei. All rights reserved.
 // Use of this source code is governed by a license
 // that can be found in the LICENSE file.
 
 var trace bool = false
-var traceLogger *log.Logger
+var traceLogger standardlog.Logger
 
 // TraceLogger enables detailed logging of Http request matching and filter invocation. Default no logger is set.
-func TraceLogger(logger *log.Logger) {
+func TraceLogger(logger standardlog.Logger) {
 	traceLogger = logger
 	trace = logger != nil
 }
